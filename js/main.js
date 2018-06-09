8dc58d9eca522adf918f3bfe97ffec5e48ab2be1
Micael Sabino <micaelsbno@gmail.com>
2018-06-10 01:40:16 +1000

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,19 +1,38 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
+checkForMatch = function() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+      console.log("You found a match");
+    } else {  
+        console.log("Try again!!");
+    }
+};
 
-var cardOne = cards[0];
-var cardTwo = cards[2];
-
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-if (cardsInPlay.length === 2;) {
-	if (cardsInPlay[0] === cardsInPlay[1])
-		alert("You found a match!");
-	else 
-		alert("Sorry, Try again!");
+var flipCard = function(cardId) {
+	console.log("User flipped " + cards[cardId])
+	cardsInPlay.push(cards[cardId]);
+    if (cardsInPlay.length === 2) {
+      checkForMatch() 
+  	};
 }
+      
 
-console.log("User flipped " + cardsInPlay);
-console.log("User flipped 	" + cardThree);
+// pop-cards
+// var cards = ["queen", "queen", "king", "king"];
+// var cardsInPlay = [];
 
+// var flipCard = function(cardId) {
+// 	console.log("User flipped " + cards[cardId])
+// 	cardsInPlay.push(cards[cardId]);
+//     if (cardsInPlay.length === 2) {
+//       if (cardsInPlay[0] === cardsInPlay [1])
+//       console.log("You have a match");
+//        else 
+//         console.log("Try again!!")
+//         cardsInPlay.pop()
+//         cardsInPlay.pop();
+//     } else
+//       console.log("Choose a new card");
+// }
+      
+ 

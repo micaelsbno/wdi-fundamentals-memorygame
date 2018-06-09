d5e72b64eed3c403e0f0ff4e54223d1d66fc6990
Micael Sabino <micaelsbno@gmail.com>
2018-06-10 03:43:48 +1000

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,20 +1,45 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+	{
+		rank: "queen",
+		suit: "hearts",
+		cardImage: "images/queen-of-hearts.png"
+	},
+	{
+		rank: "queen",
+		suit: "diamonds",
+		cardImage: "images/queen-of-diamonds.png"
+	},
+	{
+		rank: "king",
+		suit: "hearts",
+		cardImage: "images/king-of-hearts.png"
+	},
+	{
+		rank: "king",
+		suit: "diamonds",
+		cardImage: "images/king-of-diamonds.png"
+	}
+];
+
 var cardsInPlay = [];
 checkForMatch = function() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-      console.log("You found a match");
+      alert("You found a match");
     } else {  
-        console.log("Try again!!");
+        alert("Try again!!");
     }
 };
 
 var flipCard = function(cardId) {
-	console.log("User flipped " + cards[cardId])
-	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank + " of " + cards[cardId].suit)
+	console.log(cards[cardId].cardImage)
+	cardsInPlay.push(cards[cardId].rank);
     if (cardsInPlay.length === 2) {
       checkForMatch() 
   	};
 }
+
+
       
 
 // pop-cards
